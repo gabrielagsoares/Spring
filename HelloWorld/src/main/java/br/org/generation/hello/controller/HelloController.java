82dcea0648709aaa0d5a3dc7bfadd1d1d42e204f
gabrielagsoares <gabrielag.soares@otmail.com>
2021-05-27 13:32:30 -0300

    Exercícios realizados em aula

---
+++ HelloWorld/src/main/java/br/org/generation/hello/controller/HelloController.java
@@ -0,0 +1,16 @@
+package br.org.generation.hello.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController // anotação que indica que é uma classe controladora
+@RequestMapping("/hello")
+public class HelloController {
+
+	@GetMapping
+	public String sayHello(){
+		
+		return "Pretendo trabalhar minha orientação ao detalhe e minha comunicação.";
+	}
+}
---
+++ SegundaAplicacao/src/main/java/br/org/generation/SegundaAplicacao/SegundaAplicacaoApplication.java
@@ -0,0 +1,13 @@
+package br.org.generation.SegundaAplicacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SegundaAplicacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SegundaAplicacaoApplication.class, args);
+	}
+
+}
