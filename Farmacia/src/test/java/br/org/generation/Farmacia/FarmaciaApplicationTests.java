6262bccc7c9fb930f4501e774ef569faf16a45ee
gabrielagsoares <gabrielag.soares@otmail.com>
2021-06-02 18:00:47 -0300

    Exercício em aula

---
+++ Farmacia/src/test/java/br/org/generation/Farmacia/FarmaciaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.org.generation.Farmacia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmaciaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
